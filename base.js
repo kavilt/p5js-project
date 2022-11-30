c77b45852d0d8bbe88e8bbe971a2b1bd2f7ce222
kavilt <tangd46@gmail.com>
2022-11-29 18:05:01 -0700

    spacing / menu select graphics

--- base.js
+++ base.js
@@ -29,6 +29,9 @@ let songs = [];
 let title;
 let heart;
 let playButton;
+let mazeThumbnail;
+let crosshairThumbnail;
+let noteThumbnail;
 let thumbnails = []; // game3 song covers
 let previews = [];
 
@@ -53,6 +56,9 @@ function preload() {
     heart = loadImage('assets/heart.png');
     playButton = loadImage('assets/playbutton.png');
 
+    mazeThumbnail = loadImage('assets/maze.png');
+    crosshairThumbnail = loadImage('assets/crosshair.png');
+    noteThumbnail = loadImage('assets/note.png');
 }
 
 function setup() {
@@ -66,6 +72,9 @@ function setup() {
     addSongs();
     playButton.resize(45 * scalarW, 0);
 
+    mazeThumbnail.filter(INVERT);
+    crosshairThumbnail.filter(INVERT);
+    noteThumbnail.filter(INVERT);
 
 }
 
